6f1e46bcbfda1ab663dccce05e6716a088904707
Minhaz <sharif35-3001@diu.edu.bd>
2023-09-04 13:13:07 +0600

    feat: basic page structure setup

---
+++ src/app/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<header>
+				<Navbar />
+			</header>
+			<main>
+				<p className="text-red-500">Game on</p>
+			</main>
+		</>
+	);
+}
+
+export default App;
